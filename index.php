cf5f07eb0ebf7d850df78e8d02c93af104b563b5
Diogo Silva <diogosilvaoficial@gmail.com>
2018-04-18 20:26:26 -0300

    Atualização 18/04

--- index.php
+++ index.php
@@ -6,6 +6,25 @@
     <meta http-equiv="pragma" content="no-cache">
     <title>Conecta Tribos | 11 DE AGOSTO 2018 - Aviva Viagens</title>
 
+    <!-- FAVICON -->
+    <link rel="apple-touch-icon" sizes="57x57" href="includes/img/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="includes/img/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="includes/img/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="includes/img/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="includes/img/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="includes/img/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="includes/img/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="includes/img/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="includes/img/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="includes/img/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="includes/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="includes/img/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="includes/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="includes/img/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="includes/img/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
     <!--Import Google Icon Font-->
     <link rel="stylesheet " href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <!-- Carousel -->
